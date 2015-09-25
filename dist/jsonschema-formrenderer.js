177ac070325d52aef66a808f6b8cd84e0b21fa30
Aubrey Hewes <aubrey@kingsquare.nl>
2015-09-25 10:07:00 +0200

    Rebuild

--- dist/jsonschema-formrenderer.js
+++ dist/jsonschema-formrenderer.js
@@ -177,7 +177,7 @@ function renderEnum(propConfig, path, value) {
 			chunk.push('<div>');
 			chunk.push('<input type="radio" name="' + name + '" value="' + radioValue +
 			'" id="' + radioId + '"' + ((radioValue === value) ? ' checked="checked"' : '' ) +
-			'><label for="' + radioId + ''+ '">' + (propConfig.enumTitles[radioValue] || radioValue) +
+			'><label for="' + radioId + ''+ '">' + (propConfig.enumTitles[key] || radioValue) +
 			'</label>');
 			chunk.push('</div>');
 		});
@@ -189,7 +189,7 @@ function renderEnum(propConfig, path, value) {
 		$.each(propConfig['enum'], function (key, optionValue) {
 			chunk.push('<option value="' + optionValue +'"' +
 					((optionValue === value) ? ' selected="selected"' : '' ) +
-					'>' + (enumTitles[optionValue] || optionValue) + '</option>');
+					'>' + (enumTitles[key] || optionValue) + '</option>');
 		});
 		chunk.push('</select>');
 	}
