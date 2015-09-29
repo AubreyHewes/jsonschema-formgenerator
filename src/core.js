c7bddcbd7e95d0d0169370aca65ff1efd413e16d
Aubrey Hewes <aubrey@kingsquare.nl>
2015-09-29 15:59:01 +0200

    renamed internal functions + extracted logic to functions + allow immutable field rendering

--- src/core.js
+++ src/core.js
@@ -1,58 +1,5 @@
 // depends on jquery -- todo is to remove dependencies and build for multiple distribution types
 
-function renderForm (schema, path, data) {
-	var chunkPromises = [];
-	data = data || {};
-
-	if (schema.properties === undefined) {
-		if (schema.allOf) {
-			$.each(schema.allOf, function (key, subSchema) {
-				chunkPromises.push(renderForm(subSchema, path, data));
-			});
-			return renderChunks(chunkPromises);
-		}
-
-		if (schema.oneOf) {
-			$.each(schema.oneOf, function (key, subSchema) {
-				chunkPromises.push(renderForm(subSchema, path, data));
-			});
-			return renderChunks(chunkPromises);
-		}
-
-		//if (schema.$ref) {
-		//	return utils.getCachedXhrPromise('get', schema.$ref).then(function (subSchema) {
-		//		if (subSchema.properties) {
-		//			return renderForm(subSchema, path, data);
-		//		} else {
-		//			return renderChunk(path, subSchema, data);
-		//		}
-		//	});
-		//}
-
-		return renderChunks(chunkPromises);
-	}
-
-	$.each(schema.properties, function (propName, propConfig) {
-		path.slice(0);
-		path.push(propName);
-		chunkPromises.push(renderChunk(path, propConfig, data[propName]));
-	});
-	return renderChunks(chunkPromises).then(function (html) {
-
-		if (schema.required) {
-			var $html = $('<p>').html(html);
-			$.each(schema.required, function (idx, property) {
-				$html.find(['input', 'textarea', 'select'].map(function (type) {
-					return '.schema-property-' + property + ' > ' + type
-				}).join(',')).attr('required', 'required');
-			});
-			return $html.html();
-		}
-
-		return html;
-	});
-}
-
 function renderChunks(chunkPromises) {
 	return $.when.apply($, chunkPromises).then(function () {
 		return $.makeArray(arguments).join('');
@@ -86,28 +33,27 @@ function renderChunk(path, propConfig, value) {
 				chunk.push('<div class="legend">' + propConfig.title + '</div>');
 			}
 
-			chunk.push(renderForm(propConfig, subPath, value));
+			chunk.push(renderObject(propConfig, subPath, value));
 			chunk.push('</div>');
 			break;
 
 		case 'number':
+		case 'integer':
 			//check for "explicit empty"
 			if (propConfig.title && propConfig.title !== '') {
 				chunk.push('<label for="' + id + '">' + (propConfig.title ? propConfig.title : propName)  +
-					'</label>');
+						'</label>');
 			}
-			chunk.push((propConfig['enum'] === undefined) ?
-				'<input type="' + (propConfig.inputType || 'number') + '" name="' + name + '" id="' + id +
-					(propConfig.description ? '" placeholder="' + propConfig.description : '' ) +
-					'" value="' + valueAsString+ '">':
-					renderEnum(propConfig, subPath, value)
-			);
+
+			chunk.push((propConfig.format === 'immutable' ?
+				renderImmutable : renderNumber)(propConfig, subPath, value, id));
+
 			break;
 
 		case 'boolean':
 			// inputs before labels
 			chunk.push('<input type="checkbox" name="' + name + '" id="' + id + '" value="1"' +
-				(value ? ' checked="checked"' : '' ) + ' />');
+					(value ? ' checked="checked"' : '' ) + ' />');
 			chunk.push('<label for="' + id + '">' + propConfig.title + '</label>');
 			break;
 
@@ -148,26 +94,104 @@ function renderChunk(path, propConfig, value) {
 			if (isEnum) {
 				chunk.push(renderEnum(propConfig, subPath, value));
 			} else {
-				chunk.push((propConfig.inputType === 'textarea') ?
-					'<textarea type="text" name="' + name + '" id="' + id + '">' + valueAsString + '</textarea>' :
-					'<input type="' + (propConfig.inputType || propConfig.format || 'text') +
-						'" name="' + name +
-						(propConfig.description ? '" placeholder="' + propConfig.description : '' ) +
-						'" id="' + id + '" value="' + valueAsString + '">'
-				);
+				chunk.push((propConfig.format === 'immutable' ?
+						renderImmutable : renderString)(propConfig, subPath, value, id));
 			}
 			break;
 
 		default:
 			chunk.push('<label for="' + id + '">' + propConfig.title + '</label>');
 			chunk.push('<input type="' + (propConfig.inputType || propConfig.format || 'text') +
-				'" name="' + name + '" id="' + id +
-				(propConfig.description ? '" placeholder="' + propConfig.description : '' ) +
-				'" value="' + valueAsString + '">');
+					'" name="' + name + '" id="' + id +
+					(propConfig.description ? '" placeholder="' + propConfig.description : '' ) +
+					'" value="' + valueAsString + '">');
 	}
 
 	chunk.push('</div>');
 	return renderChunks(chunk);
+}
+
+function renderObject (schema, path, data) {
+	var chunkPromises = [];
+	data = data || {};
+
+	if (schema.properties === undefined) {
+		if (schema.allOf) {
+			$.each(schema.allOf, function (key, subSchema) {
+				chunkPromises.push(renderObject(subSchema, path, data));
+			});
+			return renderChunks(chunkPromises);
+		}
+
+		if (schema.oneOf) {
+			$.each(schema.oneOf, function (key, subSchema) {
+				chunkPromises.push(renderObject(subSchema, path, data));
+			});
+			return renderChunks(chunkPromises);
+		}
+
+		//if (schema.$ref) {
+		//	return utils.getCachedXhrPromise('get', schema.$ref).then(function (subSchema) {
+		//		if (subSchema.properties) {
+		//			return renderObject(subSchema, path, data);
+		//		} else {
+		//			return renderChunk(path, subSchema, data);
+		//		}
+		//	});
+		//}
+
+		return renderChunks(chunkPromises);
+	}
+
+	$.each(schema.properties, function (propName, propConfig) {
+		path.slice(0);
+		path.push(propName);
+		chunkPromises.push(renderChunk(path, propConfig, data[propName]));
+	});
+
+	return renderChunks(chunkPromises).then(function (html) {
+
+		if (schema.required) {
+			var $html = $('<p>').html(html);
+			$.each(schema.required, function (idx, property) {
+				$html.find(['input', 'textarea', 'select'].map(function (type) {
+					return '.schema-property-' + property + ' > ' + type
+				}).join(',')).attr('required', 'required');
+			});
+			return $html.html();
+		}
+
+		return html;
+	});
+}
+
+function renderString(propConfig, path, value, id) {
+	var valueAsString = value || '';
+	return (propConfig.inputType === 'textarea') ?
+		'<textarea type="text" name="' + name +
+			(propConfig.description ? '" placeholder="' + propConfig.description : '' ) +
+			'" id="' + id + '">' + valueAsString + '</textarea>' :
+		'<input type="' + (propConfig.inputType || propConfig.format || 'text') +
+			'" name="' + name +
+			(propConfig.description ? '" placeholder="' + propConfig.description : '' ) +
+			'" id="' + id + '" value="' + valueAsString + '">';
+}
+
+function renderNumber(propConfig, path, value, id) {
+	return (propConfig['enum'] === undefined) ?
+		'<input type="' + (propConfig.inputType || 'number') + '" name="' + name + '" id="' + id +
+			(propConfig.description ? '" placeholder="' + propConfig.description : '' ) +
+			(propConfig.min ? '" min="' + propConfig.min : '' ) +
+			(propConfig.max ? '" max="' + propConfig.max : '' ) +
+			'" value="' + (value || '') + '">'
+		: renderEnum(propConfig, path, value)
+}
+
+function renderImmutable(propConfig, path, value, id) {
+	if ((value === undefined) && (propConfig['default'] !== undefined)) {
+		value = propConfig['default'];
+	}
+	return '<div class="immutable">'  + value + '</div>';
 }
 
 function renderEnum(propConfig, path, value) {
@@ -180,31 +204,34 @@ function renderEnum(propConfig, path, value) {
 	}
 
 	var enumTitles = ((propConfig.options && propConfig.options.enum_titles) ?
-		propConfig.options.enum_titles : {});
+			propConfig.options.enum_titles : {});
 
+	if (propConfig.format === 'immutable') {
+		return renderImmutable(propConfig, path, enumTitles[value], id);
+	}
 	switch(propConfig.inputType) {
-	case 'radio':
-		chunk.push('<div class="radiogroup">');
-		$.each(propConfig['enum'], function (key, radioValue) {
-			var radioId = id + '-' + radioValue;
-			chunk.push('<div>');
-			chunk.push('<input type="radio" name="' + name + '" value="' + radioValue +
-			'" id="' + radioId + '"' + ((radioValue === value) ? ' checked="checked"' : '' ) +
-			'><label for="' + radioId + ''+ '">' + (propConfig.enumTitles[key] || radioValue) +
-			'</label>');
+		case 'radio':
+			chunk.push('<div class="radiogroup">');
+			$.each(propConfig['enum'], function (key, radioValue) {
+				var radioId = id + '-' + radioValue;
+				chunk.push('<div>');
+				chunk.push('<input type="radio" name="' + name + '" value="' + radioValue +
+						'" id="' + radioId + '"' + ((radioValue === value) ? ' checked="checked"' : '' ) +
+						'><label for="' + radioId + ''+ '">' + (enumTitles[key] || radioValue) +
+						'</label>');
+				chunk.push('</div>');
+			});
 			chunk.push('</div>');
-		});
-		chunk.push('</div>');
-		break;
+			break;
 
-	default:
-		chunk.push('<select name="' + name + '" id="' + id + '" class="">');
-		$.each(propConfig['enum'], function (key, optionValue) {
-			chunk.push('<option value="' + optionValue +'"' +
-					((optionValue === value) ? ' selected="selected"' : '' ) +
-					'>' + (enumTitles[key] || optionValue) + '</option>');
-		});
-		chunk.push('</select>');
+		default:
+			chunk.push('<select name="' + name + '" id="' + id + '" class="">');
+			$.each(propConfig['enum'], function (key, optionValue) {
+				chunk.push('<option value="' + optionValue +'"' +
+						((optionValue === value) ? ' selected="selected"' : '' ) +
+						'>' + (enumTitles[key] || optionValue) + '</option>');
+			});
+			chunk.push('</select>');
 	}
 
 	return chunk.join('');
