65c9996029ed5296eeb26964413183b91e3cb192
Aubrey Hewes <aubrey@kingsquare.nl>
2016-02-03 15:40:10 +0100

    rebuild

--- dist/jsonschema-formgenerator.js
+++ dist/jsonschema-formgenerator.js
@@ -154,7 +154,6 @@ function renderOneOf(schema, path, data) {
     chunks.push('<option value="' + idx + '">' + subSchema.title + '</option>');
     delete subSchema.title;
     subSchema.disabled = true;
-    console.log('oneOF adata', data);
     subSchemaChunks.push(renderObject(subSchema, path, data));
   });
 
@@ -169,7 +168,7 @@ function renderOneOf(schema, path, data) {
       var $target = $(event.target);
       $target.closest('.schema-property').siblings('.schema-property-oneOf').show()
         .find('> fieldset').attr('disabled', 'disabled').hide().each(function (idx) {
-        if (idx === parseInt($target.val(), 10)) {
+        if (idx === $target[0].selectedIndex) {
           $(this).removeAttr('disabled').show();
         }
       });
